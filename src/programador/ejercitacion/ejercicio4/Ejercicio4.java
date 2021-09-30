c9c3287f4c083f0be0a0b5ce0f632494b89a0c2b
Federico Mamoris <fgmamoris>
2021-09-30 15:25:17 -0300

    Ejercicio4 finalizado

--- src/programador/ejercitacion/ejercicio4/Ejercicio4.java
+++ src/programador/ejercitacion/ejercicio4/Ejercicio4.java
@@ -17,19 +17,14 @@ public class Ejercicio4 {
      */
     public static void main(String[] args) {
         int seleccion = 0;
+        boolean haceSol, nevado;
+        int temperatura;
+
         try (Scanner sc = new Scanner(System.in)) {
-            
+
             do {
                 System.out.println("Seleccione un ejercicio, a fin de verificar el resultado");
-                System.out.println("Ejercicio 1: ");
-                System.out.println("Ejercicio 2: ");
-                System.out.println("Ejercicio 3: ");
-                System.out.println("Ejercicio 4: ");
-                System.out.println("Ejercicio 5: ");
-                System.out.println("Ejercicio 6: ");
-                System.out.println("Ejercicio 7: ");
-                System.out.println("Ejercicio 8: ");
-                System.out.println("Ejercicio 9: ");
+                System.out.println("Ejercicio [1,2,3,4,5,6,7,8,9,10]");
                 System.out.println("Para finalizar el programa ingrese 0");
                 seleccion = sc.nextInt();
                 System.out.flush();
@@ -39,164 +34,111 @@ public class Ejercicio4 {
                     case 0:
                         break;
                     case 1:
-
-                        int n = 5;
-                        double a = 4.56;
-                        char c = 'a';
-                        System.out.println("----- RESULTADO -----");
-                        System.out.println("Valor variable n: " + n);
-                        System.out.println("Valor variable a: " + a);
-                        System.out.println("Valor variable c: " + c);
-                        System.out.println("La suma de n + a: " + (n + a));
-                        System.out.println("La diferencia de A - N: " + (a - n));
-                        System.out.println("El valor numérico correspondiente al carácter que contiene la variable C: " + (int) c);
-                        System.out.println("----------------------");
-                        do {
-                            System.out.println("¿Desea seguir consultado los ejercicios?");
-                            System.out.println("0 = [NO] - 1 = [SI]");
-                            seleccion = sc.nextInt();
-
-                            System.out.flush();
-                        } while (!(seleccion == 0 || seleccion == 1));
-
+                        temperatura = 27;
+                        if (temperatura > 25) {
+                            System.out.println("A la Playa!!!");
+                        }
                         break;
-
                     case 2:
-                        int X = 1,
-                         Y = 2;
-                        double N = 4.7,
-                         M = 3.2;
-
-                        System.out.println("------------------- RESULTADO -------------------");
-                        System.out.println("Valor variable X: " + X);
-                        System.out.println("Valor variable Y: " + Y);
-                        System.out.println("Valor variable N: " + N);
-                        System.out.println("Valor variable M: " + M);
-                        System.out.println("La suma X + Y: " + (X + Y));
-                        System.out.println("La diferencia X - Y: " + (X - Y));
-                        System.out.println("El producto X * Y: " + (X * Y));
-                        System.out.println("El cociente X / Y: " + (X / Y));
-                        System.out.println("El resto X % Y: " + (X % Y));
-                        System.out.println("La suma N + M: " + (N + M));
-                        System.out.println("La diferencia N - M: " + (N - M));
-                        System.out.println("El producto N * M: " + (N * M));
-                        System.out.println("El cociente N / M: " + (N / M));
-                        System.out.println("El resto N % M: " + (N % M));
-                        System.out.println("La suma X + N: " + (X + N));
-                        System.out.println("El cociente Y / M: " + (Y / M));
-                        System.out.println("El resto Y % M: " + (Y % M));
-                        System.out.println("El doble de la variable X: " + X + " es: " + (X * 2));
-                        System.out.println("El doble de la variable Y: " + Y + " es: " + (Y * 2));
-                        System.out.println("El doble de la variable N: " + N + " es: " + (N * 2));
-                        System.out.println("El doble de la variable M: " + M + " es: " + (M * 2));
-                        System.out.println("La suma de todas las variables es: " + (X + Y + N + M));
-                        System.out.println("El producto de todas las variables es: " + (X * Y * N * M));
-                        System.out.println("-------------------------------------------------");
-                        do {
-                            System.out.println("¿Desea seguir consultado los ejercicios?");
-                            System.out.println("0 = [NO] - 1 = [SI]");
-                            seleccion = sc.nextInt();
-                            System.out.flush();
-                        } while (!(seleccion == 0 || seleccion == 1));
+                        haceSol = true;
+                        if (haceSol == true) {
+                            System.out.println("No te olvides la sombrilla");
+                        }
                         break;
                     case 3:
-                        int A = 1,
-                         B = 2,
-                         C = 3,
-                         D = 4;
-                        System.out.println("------------- RESULTADO -------------");
-                        System.out.println("Valores iniciales");
-                        System.out.println("Valor variable A: " + A);
-                        System.out.println("Valor variable B: " + B);
-                        System.out.println("Valor variable C: " + C);
-                        System.out.println("Valor variable D: " + D);
-                        System.out.println("Valores finales");
-                        int temp = B;
-                        B = C;
-                        C = A;
-                        A = D;
-                        D = temp;
-
-                        System.out.println("B toma el valor de C -> B = " + B);
-                        System.out.println("C toma el valor de A -> C = " + C);
-                        System.out.println("A toma el valor de D -> A = " + A);
-                        System.out.println("D toma el valor de B -> D = " + D);
-                        System.out.println("-------------------------------------");
-                        do {
-                            System.out.println("¿Desea seguir consultado los ejercicios?");
-                            System.out.println("0 = [NO] - 1 = [SI]");
-                            seleccion = sc.nextInt();
-                            System.out.flush();
-                        } while (!(seleccion == 0 || seleccion == 1));
+                        haceSol = true;
+                        nevado = true;
+                        if (nevado == true || haceSol == true) {
+                            System.out.println("Que bien!!");
+                        }
                         break;
-
                     case 4:
-                        A = 14;
-                        System.out.println("------------- RESULTADO -------------");
-                        System.out.println("Operado ternario");
-                        System.out.println((A % 2 == 0) ? (A + " - es par") : (A + " - es impar"));
-                        A = 15;
-                        System.out.println((A % 2 == 0) ? (A + " - es par") : (A + " - es impar"));
-                        System.out.println("-------------------------------------");
-                        do {
-                            System.out.println("¿Desea seguir consultado los ejercicios?");
-                            System.out.println("0 = [NO] - 1 = [SI]");
-                            seleccion = sc.nextInt();
-                            System.out.flush();
-                        } while (!(seleccion == 0 || seleccion == 1));
+                        temperatura = 29;
+                        nevado = true;
+                        if ((nevado && temperatura >= 20) && temperatura <= 30) {
+                            System.out.println("No me lo creo");
+                        }
                         break;
                     case 5:
-                        B = 1;
-                        System.out.println("------------- RESULTADO -------------");
-                        System.out.println("Operado ternario");
-                        System.out.println((B >= 0) ? (B + " - es positivo") : (B + " - es negativo"));
-                        B = -1;
-                        System.out.println((B >= 0) ? (B + " - es positivo") : (B + " - es negativo"));
-                        System.out.println("-------------------------------------");
-                        System.out.println("¿Desea seguir consultado los ejercicios?");
-                        do {
-                            System.out.println("¿Desea seguir consultado los ejercicios?");
-                            System.out.println("0 = [NO] - 1 = [SI]");
-                            seleccion = sc.nextInt();
-                            System.out.flush();
-                            System.out.println(seleccion);
-                        } while (!(seleccion == 0 || seleccion == 1));
+                        temperatura = 32;
+                        haceSol = true;
+                        if (temperatura < 0 || (temperatura > 30 && haceSol)) {
+                            System.out.println("Mejor me quedo en casa");
+                        }
                         break;
                     case 6:
-                        C = 55;
-                        System.out.println("-------------------------- RESULTADO --------------------------");
-                        System.out.println("Operado ternario");
-                        System.out.println("Valor de C: " + C);
-                        System.out.println((C >= 0) ? (C + " - es positivo") : (C + " - es negativo"));
-                        System.out.println((C % 2 == 0) ? (C + " - es par") : (C + " - es impar"));
-                        System.out.println((C % 5 == 0) ? (C + " - es multiplo de 5") : (C + " - no es multiplo de 5"));
-                        System.out.println((C % 10 == 0) ? (C + " - es multiplo de 10") : (C + " - no es multiplo de 10"));
-                        System.out.println((C > 100) ? (C + " - es mayor a 100") : (C + " - es menor o igual que 100"));
-                        C = 150;
-                        System.out.println("Operado ternario");
-                        System.out.println("Valor de C: " + C);
-                        System.out.println((C >= 0) ? (C + " - es positivo") : (C + " - es negativo"));
-                        System.out.println((C % 2 == 0) ? (C + " - es par") : (C + " - es impar"));
-                        System.out.println((C % 5 == 0) ? (C + " - es multiplo de 5") : (C + " - no es multiplo de 5"));
-                        System.out.println((C % 10 == 0) ? (C + " - es multiplo de 10") : (C + " - no es multiplo de 10"));
-                        System.out.println((C > 100) ? (C + " - es mayor a 100") : (C + " - es menor o igual que 100"));
-                        System.out.println("-----------------------------------------------------------------");
-                        do {
-                            System.out.println("¿Desea seguir consultado los ejercicios?");
-                            System.out.println("0 = [NO] - 1 = [SI]");
-                            seleccion = sc.nextInt();
-                            System.out.flush();
-                        } while (!(seleccion == 0 || seleccion == 1));
+                        temperatura = 32;
+                        if (temperatura > 25) {
+                            System.out.println("A la playa!!!");
+                        }
+                        break;
+                    case 7:
+                        temperatura = 10;
+                        if (temperatura <= 25) {
+                            System.out.println("Esperando el buen tiempo..-");
+
+                        }
+                        break;
+                    case 8:
+                        temperatura = 10;
+                        if (temperatura > 25) {
+                            System.out.println("A la playa");
+
+                        } else {
+                            System.out.println("Esperando al buen tiempo...");
+                        }
+
+                        break;
+                    case 9:
+                        temperatura = 10;
+                        nevado = false;
+                        if (temperatura > 25) {
+                            System.out.println("A la playa!!");
+                        } else {
+                            if (temperatura > 15) {
+                                System.out.println("A la montaña");
+                            } else {
+                                if (temperatura < 5 && nevado) {
+                                    System.out.println("A esquiar!!!");
+                                } else {
+                                    System.out.println("A dormir");
+                                }
+                            }
+                        }
+                        break;
+                    case 10:
+                        temperatura = 10;
+                        if (temperatura < 15) {
+                            if (temperatura < 5) {
+                                System.out.println("A esquiar!!");
+                            } else {
+                                System.out.println("A dormir..zzz!");
+                            }
+
+                        } else {
+                            if (temperatura > 25) {
+                                System.out.println("A la playa!!!");
+                            } else {
+                                System.out.println("A la montaña");
+                            }
+                        }
+
                         break;
                     default:
                         System.out.println("¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡Elija una opción válida!!!!!!!");
-
                 }
+                do {
+                    System.out.println("¿Desea seguir consultado los ejercicios?");
+                    System.out.println("0 = [NO] - 1 = [SI]");
+                    seleccion = sc.nextInt();
 
+                    System.out.flush();
+                } while (!(seleccion == 0 || seleccion == 1));
             } while (seleccion != 0);
             System.out.println(
                     "Programa finalizado");
         } catch (Exception e) {
+            System.out.println("Se ha detectado un error en el sistema");
             System.out.println(e);
         }
     }
