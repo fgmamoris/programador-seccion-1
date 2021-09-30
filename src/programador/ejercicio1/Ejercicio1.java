7eb1cd641de8a1c6ea565d7329fab33090b2ebd7
Federico Mamoris <fgmamoris>
2021-09-30 13:06:23 -0300

    Ejercicio 1

---
+++ src/programador/ejercicio1/Ejercicio1.java
@@ -0,0 +1,24 @@
+/*
+ * Realizar un programa que muestre un mensaje diciendo "Hola Mundo".
+ * Acompañar este desarrollo con su correspondiente diagrama de flujo.
+ * Se debe desarrollar por consola teniendo encuentra que una vez realizado el archivo.java 
+ * debe compilarse para ejecutarse.
+
+ */
+package programador.ejercicio1;
+
+/**
+ *
+ * @author Federico Mamoris
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!!!");
+
+    }
+
+}
